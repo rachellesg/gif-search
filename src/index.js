1a9fd85e7014c479976df35d82db5d6d1dc759d8
rachellesg <rachelle.yeo@outlook.com>
2021-03-08 16:24:00 +0800

    cycle lags by 1 step, trying to fix

--- src/index.js
+++ src/index.js
@@ -1,36 +1,30 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import SearchBar from './components/searchBar';
-import axios from 'axios';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import SearchBar from "./components/searchBar";
+import axios from "axios";
 
 function App() {
-  const [payload, setPayload] = useState('');
-  const [searchLoad, setSearchLoad] = useState('');
+  const [payload, setPayload] = useState("");
+  const [searchLoad, setSearchLoad] = useState({});
 
-  const apiUrl = `http://api.giphy.com/v1/gifs/search?q=${payload}&api_key=lhi5oCffHG3ybpyeuZUmvlVqxXS5nWja`
+  const apiUrl = `http://api.giphy.com/v1/gifs/search?q=${payload}&api_key=lhi5oCffHG3ybpyeuZUmvlVqxXS5nWja`;
 
-  const fetchData = async () => {
-    const response = await axios.get(apiUrl)
-    setSearchLoad(response.data)
-    console.log('clicked')
-    console.log(response.data)
-  }
+  const fetchData = async (payLoad) => {
+    const response = await axios.get(apiUrl);
+    setSearchLoad(response.data);
+    console.log("RESPONSE");
+    console.log(response.data);
+    console.log("SEARCH LOAD");
+    console.log(searchLoad)
+  };
 
   return (
     <>
-    Hello <br />      
-    <input value={payload} onChange={(e) => setPayload(e.target.value)} />
-    <span onClick={fetchData}> Click Here To Fetch </span>
-
-    {searchLoad.map((item, index) => {
-      return (
-        <>  {item.images.downsized.url}
-        </>
-      )
-    })}
-    {/* <SearchBar payload={payload}/> */}
+      Hello <br />
+      <input value={payload} onChange={(e) => setPayload(e.target.value)} />
+      <span onClick={fetchData}>Click Here To Fetch</span> 
     </>
-  )
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
