c258af9a22857f76b87032dddcc4178af784fa53
rachellesg <rachelle.yeo@outlook.com>
2021-03-08 16:59:49 +0800

    change axios handling

--- src/index.js
+++ src/index.js
@@ -4,25 +4,30 @@ import SearchBar from "./components/searchBar";
 import axios from "axios";
 
 function App() {
-  const [payload, setPayload] = useState("");
-  const [searchLoad, setSearchLoad] = useState({});
+  const [searchLoad, setSearchLoad] = useState([]);
+  const [payLoad, setPayLoad] = useState("");
 
-  const apiUrl = `http://api.giphy.com/v1/gifs/search?q=${payload}&api_key=lhi5oCffHG3ybpyeuZUmvlVqxXS5nWja`;
+  const apiUrl = `http://api.giphy.com/v1/gifs/search`;
+  const apiKey = "lhi5oCffHG3ybpyeuZUmvlVqxXS5nWja";
 
-  const fetchData = async (payLoad) => {
-    const response = await axios.get(apiUrl);
+  const fetchData = async () => {
+    const response = await axios.get(apiUrl, {
+      params: { q: payLoad, api_key: apiKey },
+    });
     setSearchLoad(response.data);
     console.log("RESPONSE");
     console.log(response.data);
     console.log("SEARCH LOAD");
-    console.log(searchLoad)
+    console.log(searchLoad.data);
   };
 
   return (
     <>
       Hello <br />
-      <input value={payload} onChange={(e) => setPayload(e.target.value)} />
-      <span onClick={fetchData}>Click Here To Fetch</span> 
+      <input value={payLoad} onChange={(e) => setPayLoad(e.target.value)} />
+      <span onClick={fetchData}>Click Here To Fetch</span>
+      {payLoad}
+      {/* { searchLoad && searchLoad.map(item => <>{item.image.downsized.url}</>)} */}
     </>
   );
 }
